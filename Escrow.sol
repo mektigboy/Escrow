bf90a035a00413d2b5ce9636711923b21332e8cb
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-17 23:09:48 -0500

    Update Escrow.sol

--- Escrow.sol
+++ Escrow.sol
@@ -5,7 +5,7 @@ pragma solidity ^ 0.8.13;
 contract Escrow {
   address agent; // Officiates transaction on the blockchain. Only person allowed to deposit funds into smart contract and withdraw them.
 
-  mapping(address => uint) public deposits;
+  mapping(address => uint) public deposits; // Deposits in escrow.
 
   modifier onlyAgent() {
     require(msg.sender == agent);
