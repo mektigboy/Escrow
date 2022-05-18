4a411431b1969bbc39a9f2f2c9af6f255cfdcd9d
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-17 22:52:02 -0500

    Update Escrow.sol

--- Escrow.sol
+++ Escrow.sol
@@ -3,5 +3,9 @@
 pragma solidity ^ 0.8.13;
 
 contract Escrow {
-  
+  address = msg.sender; // Officiates transaction on the blockchain. Only person allowed to deposit funds into smart contract and withdraw them.
+
+  constructor() public {
+
+  }
 }
